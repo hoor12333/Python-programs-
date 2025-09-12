adb11872b1a586bae7850e4529385a0eafef495a
hoor12333 <hoorainfatema4@gmail.com>
2025-09-12 12:52:42 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+
+import calc
+
+print("Addition:", calc.add(10, 5))
+print("Subtraction:", calc.subtract(10, 5))
+print("Multiplication:", calc.multiply(10, 5))
+print("Division:", calc.divide(10, 5))
+print("Division by zero:", calc.divide(10, 0))
+
